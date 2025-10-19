4c15e3a79104f91e39ce7347d789d48b6b4c4d73
hema <hemakhati007@gmail.com>
2025-10-19 21:05:57 +0530

    feat: add automatic types from Sanity for improved type safety

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard,{StartupTypeCard} from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+
+
 
 export default async function Home({searchParams}:{searchParams:Promise<{query?:string}>}) {
 
@@ -15,34 +17,37 @@ export default async function Home({searchParams}:{searchParams:Promise<{query?:
  
 
   return (
-   <>
-   <section className="pink_container">
-   
-    <h1 className="heading">Pitch Your Startup,<br/> Connect With Enterpreneurs</h1>
-    <p className="sub-heading !max-w-3xl">
-      Submit Ideas,Vote on Pitches, and Get Noticed In Virtual Competitions.
-    </p>
-    <SearchForm query={query}/>
-    {/* need to difien query rypw in serachfom componnet otherwise it wont recognize here */}
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup,
+          <br /> Connect With Enterpreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas,Vote on Pitches, and Get Noticed In Virtual Competitions.
+        </p>
+        <SearchForm query={query} />
+        {/* need to difien query rypw in serachfom componnet otherwise it wont recognize here */}
+      </section>
 
-   </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+        {/* we can map our posts */}
 
-    <section className="section_container">
-      <p className="text-30-semibold">
-        {query?`Search results for ${query}`:'All Startups'}
-      </p>
-      {/* we can map our posts */}
-      
-      <ul className="mt-7 card_grid">
-        {posts?.length>0?(posts.map((post: StartupCardType, index: number)=>(
-        
-          // passing an index is not a good practice  so we will pass post id cuz its gonna be always unique
-          <StartupCard key={post?._id} post={post}/>
-        ))):(<p className="no-results">No Startups Found</p>)}
-      </ul>
-
-    </section>
-     </>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              // passing an index is not a good practice  so we will pass post id cuz its gonna be always unique
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
 // ! before class to override other styles previously provided
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,11 +3,32 @@ import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { Author, Startup } from "@/sanity/types";
 //accept the props
 
+
+
+//import form types.ts
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+//omit by tyepescript that construct a type with properites of T escept for those in type K
+
+
+    // author: { _id: authorId, name },
+// instead of destructuring author from post as they canbe otional? we just use them directly
+//properties that can be undefine may show error so use ? to condition if exist
+
 //instead of post. anything lets destructure the data at the start
-const StartupCard = ({ post }: { post: StartupCardType }) => {
-  const {_createdAt, views, author: {_id:authorId, name}, title, category, _id, description,image} = post;
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    title,
+    author,
+    category,
+    _id,
+    description,
+    image,
+  } = post;
   return (
     <li className="startup-card group">
       <div className="flex-between">
@@ -20,14 +41,14 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
-            <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${author?.id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold line_clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?.id}`}>
           {/* need to configue the palcehold.co <Image> component doesn’t allow external images unless you explicitly allow their domains in next.config.js.   */}
           <Image
             src="https://placehold.co/48x48"
@@ -39,9 +60,7 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
         </Link>
       </div>
       <Link href={`/startup/${_id}`}>
-        <p className="startup_card_desc">
-            {description}
-        </p>
+        <p className="startup_card_desc">{description}</p>
         <Image
           src={image}
           alt="startup image"
@@ -54,19 +73,16 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
       </Link>
 
       <div className="flex-between gap-3 mt-5">
-        <Link href={`/?query=${category.toLowerCase()}`}>
+        <Link href={`/?query=${category?.toLowerCase()}`}>
           {/* update the query of url based on category of post  */}
           <p className="text-16-medium">{category}</p>
         </Link>
         <Button className="startup_card_btn" asChild>
           {/* asChild bcz of the link in it */}
-          <Link href={`/startup/${_id}`}>
-            Details
-          </Link>
+          <Link href={`/startup/${_id}`}>Details</Link>
         </Button>
         {/* shadcn button */}
       </div>
-
     </li>
   );
 };
