2c8722fa282416c65d6f54234bd304ece184c5a9
hema <hemakhati007@gmail.com>
2025-10-23 22:29:21 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ const workSans=localFont({
       style:'normal'
     },
        {
-      path:'./fonts/WorkSans-thin.ttf',
+      path:'./fonts/WorkSans-Thin.ttf',
       weight:'200',
       style:'normal'
     },
